25d7718890437d1968383dcfc36837c7206460d3
mmyersDE <mathias.rudolph@remondis.de>
2015-11-10 10:12:17 +0100

    Switched to jsonv2 and added field 'place_rank'

--- src/main/java/fr/dudie/nominatim/client/JsonNominatimClient.java
+++ src/main/java/fr/dudie/nominatim/client/JsonNominatimClient.java
@@ -158,9 +158,9 @@ public final class JsonNominatimClient implements NominatimClient {
         } catch (UnsupportedEncodingException e) {
             emailEncoded = email;
         }
-        this.searchUrl = String.format("%s/search?format=json&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
-        this.reverseUrl = String.format("%s/reverse?format=json&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
-        this.lookupUrl = String.format("%s/lookup?format=json&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
+        this.searchUrl = String.format("%s/search?format=jsonv2&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
+        this.reverseUrl = String.format("%s/reverse?format=jsonv2&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
+        this.lookupUrl = String.format("%s/lookup?format=jsonv2&email=%s", baseUrl.replaceAll("/$", ""), emailEncoded);
 
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("API search URL: {}", searchUrl);
